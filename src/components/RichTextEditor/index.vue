c83ecd5df67a8a9f7d1a64b2988cd8ced1f76bea
harkinli <harkinli@tencent.com>
2021-12-19 17:46:27 +0800

    feat: 初始化提交

---
+++ src/components/RichTextEditor/index.vue
@@ -0,0 +1,20 @@
+<template>
+    
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+import { quillEditor } from 'vue-quill-editor';
+
+export default defineComponent({
+    components: {
+        quillEditor
+    }
+    setup() {
+        
+    },
+});
+</script>
